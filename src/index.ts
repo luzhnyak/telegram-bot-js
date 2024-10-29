804fe1289e8fdd8764640a6ae7abdc3f73a7d7be
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-10-30 00:32:55 +0200

    init grammy app

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import { Bot } from "grammy";
+
+dotenv.config();
+
+const bot = new Bot(process.env.TG_API_KEY || "");
+
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+
+bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
+bot.start();
